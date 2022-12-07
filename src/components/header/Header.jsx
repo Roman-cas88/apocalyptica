6ff21ae27f6c139c158b5d164a885f2a0f253fe8
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2022-12-07 18:08:11 +0200

    add components/header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderTop from './HeaderTop';
+import HeaderMain from './HeaderMain'
+
+export default function Header() {
+  return (
+    <div>
+      <HeaderTop />
+      <HeaderMain />
+    </div>
+  )
+}
---
+++ src/components/header/HeaderTop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HeaderTop() {
+  return (
+    <div     style={{color: "white",
+    backgroundColor: 'black',
+    height: '25px'
+    }}>
+      HeaderTop
+    </div>
+  )
+}
