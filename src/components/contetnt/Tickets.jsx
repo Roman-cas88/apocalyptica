8ef0882132e84be2d012acf73d10d1dfc26e7841
Roman-cas88 <orekhovskiy.roman@gmail.com>
2022-12-14 23:37:09 +0100

    create cards E-Ticket

--- src/components/contetnt/Tickets.jsx
+++ src/components/contetnt/Tickets.jsx
@@ -1,4 +1,23 @@
+import { Col, Container, Row } from "react-bootstrap"
+import { CardTicket } from "./CardTicket"
+
 
 export const Tickets = () => {
-    return <h2>Tickets</h2>
+    return (
+        <>
+    <h2>Tickets</h2>
+    <Container>
+        <Row>
+            <Col><CardTicket id="0"/></Col>
+            <Col><CardTicket id="1"/></Col>
+            <Col><CardTicket id="2"/></Col>
+            <Col><CardTicket id="3"/></Col>
+            <Col><CardTicket id="4"/></Col>
+            <Col><CardTicket id="5"/></Col>
+            <Col><CardTicket id="6"/></Col>
+            <Col><CardTicket id="7"/></Col>
+        </Row>
+    </Container>
+    </>
+    )
 }
