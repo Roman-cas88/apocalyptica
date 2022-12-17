807234aa74df2b96c6803c615863334f636db558
Roman-cas88 <orekhovskiy.roman@gmail.com>
2022-12-17 16:55:27 +0100

    Update CardTicket.jsx

--- src/components/contetnt/Cards/CardTicket.jsx
+++ src/components/contetnt/Cards/CardTicket.jsx
@@ -6,9 +6,16 @@ export const CardTicket = ({ id }) => {
     let clickedCard = () => {
       console.log("Clicked")
     }
+    const SoldOut = () => {
+      const sold = ticket.isSold
+      if (sold === "true")  {return (
+        <div className="soldOut">Sold Out</div>
+      )}
+    }
     return (
         <Card style={{ width: '15rem', textAlign:'center',cursor:"pointer" }} onClick={clickedCard}>
       <Card.Img variant="top" src={ticket.img} />
+      <SoldOut />
       <Card.Body>
         <Card.Title style={{fontSize: "15px", fontWeight:"lighter"}}>{ticket.title}</Card.Title>
         <Card.Text className="mt-3" style={{fontWeight:"bold"}}>{ticket.price}</Card.Text>
