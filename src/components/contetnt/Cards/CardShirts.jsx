2631c2f3142e384f4d947569e1bb2bb04530ff1a
Roman-cas88 <orekhovskiy.roman@gmail.com>
2022-12-17 16:09:45 +0100

    check on sold

--- src/components/contetnt/Cards/CardShirts.jsx
+++ src/components/contetnt/Cards/CardShirts.jsx
@@ -1,19 +1,27 @@
 import data from "./data.json"
 import { Card } from 'react-bootstrap'
+import "./cards.css"
 
 export const CardShirts = ({ id }) => {
     let shirts = data.shirts[id]
     let clickedCard = () => {
       console.log("Clicked")
     }
+    const SoldOut = () => {
+      const sold = shirts.isSold
+      if (sold === "true")  {return (
+        <div className="soldOut">Sold Out</div>
+      )}
+    }
     // Need to tune CSS or BS
     return (
         <Card style={{ width: '15rem', height:'20rem', textAlign:'center',cursor:"pointer" }} onClick={clickedCard}>
-      <Card.Img variant="top" src={shirts.img} />
-      <Card.Body>
-        <Card.Title style={{fontSize: "15px", fontWeight:"lighter"}}>{shirts.title}</Card.Title>
-        <Card.Text className="mt-3" style={{fontWeight:"bold"}}>{shirts.price}</Card.Text>
-      </Card.Body>
-    </Card>
-    )
+          <Card.Img style={{position:"relative"}} variant="top" src={shirts.img} />
+          <SoldOut />
+          <Card.Body>
+            <Card.Title style={{fontSize: "15px", fontWeight:"lighter"}}>{shirts.title}</Card.Title>
+            <Card.Text className="mt-3" style={{fontWeight:"bold"}}>{shirts.price}</Card.Text>
+          </Card.Body>
+        </Card>
+      )
 }
