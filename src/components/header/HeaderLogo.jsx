5ce20573a02be6581e2450f3bb83ef390925687f
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2022-12-30 10:18:42 +0200

    was added Link onHeaderLogo

--- src/components/header/HeaderLogo.jsx
+++ src/components/header/HeaderLogo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function HeaderLogo() {
   const url = "https://d1lhyycl5p8pom.cloudfront.net/media/image/ff/be/cd/apocalyptica.png";
   return (
     <div >
-      <img src={url} alt="Official Apocalyptica Shop" style={{display: "block", height: "auto"}}></img>
+      <Link to="/">
+      <img src={url} alt="Official Apocalyptica Shop" style={{display: "block", height: "auto"}} />
+      </Link>
     </div>
   )
 }
