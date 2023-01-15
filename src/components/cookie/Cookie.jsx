c1bc0b9a0d155b8437d040ccd87cabd5a9691126
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2023-01-15 14:50:04 +0200

    format file

--- src/components/cookie/Cookie.jsx
+++ src/components/cookie/Cookie.jsx
@@ -3,7 +3,6 @@
 
 import { useState, useEffect } from "react";
 import CookieConsent from "react-cookie-consent";
-import HeaderSearch from '../header/HeaderSearch';
 
 export const Cookie = () => {
     const [isShow, setIsShow] = useState(false);
@@ -18,27 +17,27 @@ export const Cookie = () => {
     }, []);
 
     return (
-        <div >
-         {isShow && (
-        <CookieConsent 
-        debug
-        location="bottom"
-        buttonText= "ACCEPT"
-        cookieName="myCookie"
-        style={{display: "block",  fontSize: "14px", color: "grey", background: "#f5f5f5"}}
-        buttonStyle= {{width: "40%", color: "white", fontSize: "1rem", background: "#1e4f7c" }}
-        expires={14}
-        overlay
-        enableDeclineButton 
-        flipButtons
-        declineButtonText ="DENY"
-        declineButtonStyle = {{width: "40%", color: "white", fontSize: "1rem", background: "#1e4f7c" }}
-        buttonWrapperClasses = "w-100 btn"       
-        >
-        This site uses third-party website tracking technologies to provide and continually improve our services, and to display advertisements according to users' interests. I agree and may revoke or change my consent at any time with effect for the future.
-        </CookieConsent>
-         )}
-        </div>
+        <>
+            {isShow && (
+                <CookieConsent
+                    debug
+                    location="bottom"
+                    buttonText="ACCEPT"
+                    cookieName="myCookie"
+                    style={{ display: "block", fontSize: "14px", color: "grey", background: "#f5f5f5" }}
+                    buttonStyle={{ width: "40%", color: "white", fontSize: "1rem", background: "#1e4f7c" }}
+                    expires={14}
+                    overlay
+                    enableDeclineButton
+                    flipButtons
+                    declineButtonText="DENY"
+                    declineButtonStyle={{ width: "40%", color: "white", fontSize: "1rem", background: "#1e4f7c" }}
+                    buttonWrapperClasses="w-100 btn"
+                >
+                    This site uses third-party website tracking technologies to provide and continually improve our services, and to display advertisements according to users' interests. I agree and may revoke or change my consent at any time with effect for the future.
+                </CookieConsent>
+            )}
+        </>
     )
 }
 
