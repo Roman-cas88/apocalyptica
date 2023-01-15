15d31ee6d5ec5637af6fbfa7621efcfa1f3d1e82
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2023-01-15 10:58:46 +0200

    added added Cookie, delete react gid system add Header

--- src/components/cookie/Cookie.js
+++ src/components/cookie/Cookie.js
@@ -1,40 +1,27 @@
 // npm install react-cookie-consent
-import { useState } from "react";
+// https://mastermindzh.github.io/react-cookie-consent/?path=/docs/cookieconsent--default
+// import React from 'react';
 import CookieConsent from "react-cookie-consent";
 
 export const Cookie = () => {
 
     return (
         <CookieConsent 
-        debug = 'true'
+        debug
         location="bottom"
         buttonText= "ACCEPT"
         cookieName="myCookie"
-        style={{fontSize: "14px", color: "grey", background: "#f5f5f5"}}
-        buttonStyle={{ color: "white", fontSize: "1rem", background: "#1e4f7c" }}
+        style={{display: "block",  fontSize: "14px", color: "grey", background: "#f5f5f5"}}
+        buttonStyle= {{width: "40%", color: "white", fontSize: "1rem", background: "#1e4f7c" }}
         expires={1}
         overlay
         enableDeclineButton 
-        onDecline={() => {
-            // alert("nay!");
-          }}
         flipButtons
         declineButtonText ="DENY"
-        declineButtonStyle = {{  color: "white", fontSize: "1rem", background: "#1e4f7c" }}
+        declineButtonStyle = {{width: "40%", color: "white", fontSize: "1rem", background: "#1e4f7c" }}
+        buttonWrapperClasses = "w-100 btn"       
         >
         This site uses third-party website tracking technologies to provide and continually improve our services, and to display advertisements according to users' interests. I agree and may revoke or change my consent at any time with effect for the future.
         </CookieConsent>
     )
 }
-
-{/* <CookieConsent ButtonComponent="button"
-     ButtonComponentStyle={
-     color: "#ffff",
-     background: "#245564",
-     padding: "10px 20px",
-     borderRadius: "16px",
-     fontSize: "1em",
-     display: "flex",
-     alignContent: "center",
-     }>
-     </CookieConsent> */}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import NavBar from '../navbar/NavBar';
+import HeaderTop from './HeaderTop';
+import HeaderLogo from './HeaderLogo';
+import HeaderNavIcons from './HeaderNavIcons';
+
+export function Header() {
+  return (
+    <Container className='px-0 my-0 pb-3' >
+    <Row >
+    <Col md={12} className="d-none d-md-block ">
+    <HeaderTop />
+    </Col>
+    <Col xs={12} md={8} >
+      <HeaderLogo />
+    </Col>
+    <Col
+      xs={{ span: 8, order: 2 }}
+      md={{ span: 4, order: 1 }}
+    >
+      <HeaderNavIcons />
+    </Col>
+    <Col
+      xs={{ span: 4, order: 1 }}
+      md={{ span: 12, order: 2 }}
+    >
+      <NavBar />
+    </Col>
+  </Row>
+  </Container>
+  )
+}
+
+
