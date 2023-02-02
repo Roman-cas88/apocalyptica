79e9e43e5f981f56be5470b8e3975e68989de70b
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2023-02-02 22:15:52 +0200

    added dinamic show result

--- src/components/header/HeaderSearch.js
+++ src/components/header/HeaderSearch.js
@@ -1,20 +1,15 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { BsSearch } from "react-icons/bs";
 import { Form, InputGroup, Container } from "react-bootstrap/";
 import { PopoverSearchList } from "./PopoverSearchList";
 
   export default function HeaderSearch() {
     const [target, setTarget] = useState(null);
-    const [isShownSearchRes, setisShownSearchRes] =useState(false);
     const [seachInput, setSeachInput] = useState('');
 
-    const handleClick = event => {
-      setisShownSearchRes(current => !current)
-      setTarget(event.target);
-    }
     function handleChange(e) {
       setSeachInput(e.target.value);
-      // handleClick()
+      setTarget(e.target);
     }
 
     return (
@@ -29,10 +24,10 @@ import { PopoverSearchList } from "./PopoverSearchList";
           aria-describedby="search-sec"
           onChange={handleChange}
           />
-        <InputGroup.Text onClick={handleClick} id="search-sec" style={{ backgroundcolor: 'transparent'}}><BsSearch size={22}/></InputGroup.Text>
+        {/* <InputGroup.Text onClick={handleClick} id="search-sec" style={{ backgroundcolor: 'transparent'}}><BsSearch size={22}/></InputGroup.Text> */}
         </Form>
         </InputGroup>
-        {isShownSearchRes && (<PopoverSearchList show={true} target={target} seachInput={seachInput} />)}
+        <PopoverSearchList target={target} seachInput={seachInput} />
       </Container>
     )
 }
--- src/components/header/PopoverSearchList.jsx
+++ src/components/header/PopoverSearchList.jsx
@@ -1,12 +1,19 @@
 // import { match } from 'assert';
 import React, { useState, useRef } from 'react';
+import { useEffect } from 'react';
 import { Nav, Overlay, Popover } from "react-bootstrap/";
 import data from "../contetnt/Cards/data.json";
 import { SearchedItem } from './SearchedItem'
 
 export const PopoverSearchList = (props) => {
   const ref = useRef(null);
-  const [show, setShow] = useState(props.show);
+   const [show, setShow] = useState(false);
+  //  const [isSearch , setIsSearch] = useState(false);
+ 
+    useEffect(() => {
+      setShow(props.seachInput ? true : false)
+
+    },[props.seachInput] )
 
   return (
     <div ref={ref} >
@@ -17,8 +24,8 @@ export const PopoverSearchList = (props) => {
         container={ref}
         containerPadding={20}
       >
-        <Popover id="popover-contained" onClick={() => setShow(false)}>
-          <Popover.Header as="h3">Search Result:</Popover.Header>
+        <Popover id="popover-contained" onClick={() => setShow(false)} className="mw-100">
+          <Popover.Header as="h3">`Search Result: {}`</Popover.Header>
           <Popover.Body className='p-0 '>
             <SearchList text={props.seachInput} />
           </Popover.Body>
@@ -40,9 +47,9 @@ const SearchList = (props) => {
   return allProducts.map((item) => {
     return (
       <Nav.Item key={item.id}>
-        {item.title.toLowerCase().includes(text.toLowerCase()) && <SearchedItem item={item} />}
+        {item.title.toLowerCase().includes(text.toLowerCase()) && <SearchedItem item={item} /> }
       </Nav.Item>
     )
-  })
+  } )
 };
 
