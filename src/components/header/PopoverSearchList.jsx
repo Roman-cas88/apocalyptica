d096c79acecc51c18c2dbc968d0972fe0f60481f
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2023-02-03 22:45:56 +0200

    added result in footer search box

--- src/components/header/PopoverSearchList.jsx
+++ src/components/header/PopoverSearchList.jsx
@@ -8,7 +8,7 @@ import { SearchedItem } from './SearchedItem'
 export const PopoverSearchList = (props) => {
   const ref = useRef(null);
    const [show, setShow] = useState(false);
-  //  const [isSearch , setIsSearch] = useState(false);
+   const [resFooter , setResFooter] = useState(0);
  
     useEffect(() => {
       setShow(props.seachInput ? true : false)
@@ -20,14 +20,15 @@ export const PopoverSearchList = (props) => {
       <Overlay
         show={show}
         target={props.target}
-        placement="bottom"
+        placement="bottom-end"
         container={ref}
         containerPadding={20}
       >
         <Popover id="popover-contained" onClick={() => setShow(false)} className="mw-100">
-          <Popover.Header as="h3">`Search Result: {}`</Popover.Header>
+          {/* <Popover.Header as="h3">`Search Result: {}`</Popover.Header>                // No results found */}
           <Popover.Body className='p-0 '>
-            <SearchList text={props.seachInput} />
+            <SearchList text={props.seachInput} setResFooter={setResFooter}/>
+            <PopoverFooter resFooter={resFooter}/>
           </Popover.Body>
         </Popover>
       </Overlay>
@@ -43,13 +44,30 @@ const SearchList = (props) => {
   const { hoods } = data;
   const { stuff } = data;
   const  allProducts = [...girls,...ticket,...shirts,...hoods,...stuff];
+  let i = 0;
 
   return allProducts.map((item) => {
+  let res = item.title.toLowerCase().includes(text.toLowerCase());
+  i= res ? i+1 : i;
+  props.setResFooter(i);
     return (
+
       <Nav.Item key={item.id}>
-        {item.title.toLowerCase().includes(text.toLowerCase()) && <SearchedItem item={item} /> }
+        {res && i<=6 && <SearchedItem item={item}  /> }
       </Nav.Item>
+
     )
-  } )
+  } 
+  )
 };
+
+const PopoverFooter = (props) => {
+const foot = props.resFooter == 0 ? `No results found` : `${props.resFooter} results`;
+
+return (
+  <Nav className='p-2 w-100 justify-content-center'>
+   {foot}
+   </Nav>
+)
+}
 
